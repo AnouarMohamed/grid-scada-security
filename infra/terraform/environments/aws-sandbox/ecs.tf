c33db044483c8a4118f15778dfe0064f6262feb8
AnouarMohamed <m.anouar@mundiapolis.ma>
2026-09-19 23:50:50 +0100

    fix(aws): configure Cloud Map health checks

--- infra/terraform/environments/aws-sandbox/ecs.tf
+++ infra/terraform/environments/aws-sandbox/ecs.tf
@@ -48,7 +48,9 @@ resource "aws_service_discovery_service" "service" {
     routing_policy = "MULTIVALUE"
   }
 
-  health_check_custom_config {}
+  health_check_custom_config {
+    failure_threshold = 1
+  }
 }
 
 locals {
